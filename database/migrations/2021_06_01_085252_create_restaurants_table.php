7f4a69f3bd9cfdeca5e4d6a7d3e1a065e033012a
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-06-03 20:25:50 +0300

    add description column to restaurant

--- database/migrations/2021_06_01_085252_create_restaurants_table.php
+++ database/migrations/2021_06_01_085252_create_restaurants_table.php
@@ -17,6 +17,7 @@ class CreateRestaurantsTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('address');
+            $table->text('description');
             $table->string('image');
             $table->string('slug');
             $table->timestamps();
