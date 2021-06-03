92b91a339f0c327a3cbc9f0d1530bcec54b090a1
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-06-03 13:14:10 +0300

    create route for home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'home'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
