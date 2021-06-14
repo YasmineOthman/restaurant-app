f560ea1985f0a747f9870e2f68b9e1303dbd355c
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-06-14 17:14:55 +0300

    test to fix

---
+++ resources/views/Reports.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Export Reports as excel file</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+</head>
+<body>
+   
+<div class="container">
+    <div class="card bg-light mt-3">
+        <div class="card-header">
+    Export reports 
+        </div>
+        <div class="card-body">
+         
+              
+                <a class="btn btn-warning" href="{{ route('export') }}">Export User orders Data</a>
+       
+        </div>
+    </div>
+</div>
+   
+</body>
+</html>
