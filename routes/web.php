bf01caf59185e2614604f294c1953909d163665a
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-06-16 16:09:14 +0300

    add route for reservation

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ComponentController;
 use App\Http\Controllers\MealOrderController;
 use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\ExportReportController;
 
 /*
@@ -24,25 +25,62 @@ use App\Http\Controllers\ExportReportController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//home
 Route::get('/', [PageController::class, 'home'])->name('home');
+//resource
 Route::resource('restaurants', RestaurantController::class);
 Route::resource('categories', CategoryController::class);
 Route::resource('meals', MealController::class);
 Route::resource('components',ComponentController::class);
 Route::resource('orders',OrderController::class);
 Route::resource('tables',TableController::class);
-Route::post('/mealorder/{id}',[MealOrderController::class,'storeorder'])->name('meal-order.storeorder');
+Route::resource('mealorder',MealOrderController::class);
+Route::resource('reservations',ReservationController::class);
+
+//functions
 Route::get('/order/{id}',[OrderController::class,'createorder'])->name('res-order.createorder');
 Route::get('/table/{id}',[TableController::class,'createtable'])->name('res-table.createtable');
+Route::get('/reservation/{id}',[ReservationController::class,'createreservation'])->name('res-reservation.createreservation');
+Route::post('/mealorder/{id}',[MealOrderController::class,'storeorder'])->name('meal-order.storeorder');
 Route::post('/tables',[TableController::class,'storetable'])->name('res-table.storetable');
 Route::get('/restaurant',[RestaurantController::class,'search'])->name('restaurants.search');
 Route::get('/category',[CategoryController::class,'search'])->name('categories.search');
 Route::get('/meal',[MealController::class,'search'])->name('meals.search');
-
 Route::get("/chart", [ChartController::class,'Chart'])->name('chart');
 Route::get('importExportView', [ExportReportController::class, 'importExportView']);
 Route::get('export', [ExportReportController::class, 'export'])->name('export');
+
+//dashboard
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Route::get('/order/{id}',[OrderController::class,'order'])->name('res-order.order');;
 
@@ -56,9 +94,3 @@ Route::get('export', [ExportReportController::class, 'export'])->name('export');
 //     return view ('order.create',['meals'=>$meals,'restaurant'=>$restaurant]);
     // return view('pages.post', ['post' => $post]);
 // });
-Route::resource('mealorder',MealOrderController::class);
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
