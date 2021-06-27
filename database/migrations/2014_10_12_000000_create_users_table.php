e3411047b0ff78c7db8db91a30cc3b23ef62be4d
Fadia AlMatar <80985155+FadiaAlmatar@users.noreply.github.com>
2021-06-27 22:52:58 +0300

    fix some issue

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->boolean('role_id')->default(false);
+            // $table->boolean('role_id')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2021_06_25_215300_add_restaurant_id_to_salelogs_table.php
+++ database/migrations/2021_06_25_215300_add_restaurant_id_to_salelogs_table.php
@@ -4,11 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD:database/migrations/2021_06_25_215300_add_restaurant_id_to_salelogs_table.php
 class AddRestaurantIdToSalelogsTable extends Migration
-=======
-class AddDonationToOrdersTable extends Migration
->>>>>>> 1e239e25bd82ced27c12c58700dc88eca4296050:database/migrations/2021_06_20_015957_add_donation_to_orders_table.php
+// class AddDonationToOrdersTable extends Migration
+// >>>>>>> 1e239e25bd82ced27c12c58700dc88eca4296050:database/migrations/2021_06_20_015957_add_donation_to_orders_table.php
 {
     /**
      * Run the migrations.
@@ -17,15 +15,13 @@ class AddDonationToOrdersTable extends Migration
      */
     public function up()
     {
-<<<<<<< HEAD:database/migrations/2021_06_25_215300_add_restaurant_id_to_salelogs_table.php
         Schema::table('salelogs', function (Blueprint $table) {
             //
-            $table->foreignId('restaurant_id')->references('id')->on('restaurants')->constrained()->onDelete('cascade');          
+            $table->foreignId('restaurant_id')->references('id')->on('restaurants')->constrained()->onDelete('cascade');
 
-=======
-        Schema::table('orders', function (Blueprint $table) {
-            $table->integer('donation')->default(0);
->>>>>>> 1e239e25bd82ced27c12c58700dc88eca4296050:database/migrations/2021_06_20_015957_add_donation_to_orders_table.php
+//         Schema::table('orders', function (Blueprint $table) {
+//             $table->integer('donation')->default(0);
+// >>>>>>> 1e239e25bd82ced27c12c58700dc88eca4296050:database/migrations/2021_06_20_015957_add_donation_to_orders_table.php
         });
     }
 
@@ -36,14 +32,12 @@ class AddDonationToOrdersTable extends Migration
      */
     public function down()
     {
-<<<<<<< HEAD:database/migrations/2021_06_25_215300_add_restaurant_id_to_salelogs_table.php
         Schema::table('salelogs', function (Blueprint $table) {
             //
             $table->dropColumn('restaurant_id');
-=======
-        Schema::table('orders', function (Blueprint $table) {
-            $table->dropColumn('donation');
->>>>>>> 1e239e25bd82ced27c12c58700dc88eca4296050:database/migrations/2021_06_20_015957_add_donation_to_orders_table.php
+//         Schema::table('orders', function (Blueprint $table) {
+//             $table->dropColumn('donation');
+// >>>>>>> 1e239e25bd82ced27c12c58700dc88eca4296050:database/migrations/2021_06_20_015957_add_donation_to_orders_table.php
         });
     }
 }
